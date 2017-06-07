644d268f3004225b74c2dcc2c2dbec402a1207eb
fgh345 <1145073867@qq.com>
2017-06-07 10:17:22 +0800

    ..

--- app/src/main/java/com/sinoangel/ctrl/parentalcontrol/curriculumschedule/CurriculumScheduleActivity.java
+++ app/src/main/java/com/sinoangel/ctrl/parentalcontrol/curriculumschedule/CurriculumScheduleActivity.java
@@ -1,11 +1,9 @@
 package com.sinoangel.ctrl.parentalcontrol.curriculumschedule;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.View;
 
 import com.sinoangel.ctrl.parentalcontrol.R;
 import com.sinoangel.ctrl.parentalcontrol.base.BaseActivity;
@@ -23,49 +21,20 @@ public class CurriculumScheduleActivity extends BaseActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_curriculumschedule);
+        setContentView(R.layout.activity_curriculumschedule);
 
-        int id = getIntent().getIntExtra("aa", 0);
-        if (id == 0)
-            setContentView(R.layout.zzz_item1);
-        else if (id == 1)
-            setContentView(R.layout.zzz_item4);
-        else if (id == 2)
-            setContentView(R.layout.zzz_item2);
-        else
-            setContentView(R.layout.zzz_item3);
+        rv_list = (RecyclerView) findViewById(R.id.rv_list);
 
-//        rv_list = (RecyclerView) findViewById(R.id.rv_list);
-//
-//        LinearLayoutManager llm = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
-//        rv_list.setLayoutManager(llm);
-//
-//        rv_list.setAdapter(curriculumAdapter);
-//
-//        getData();
+        LinearLayoutManager llm = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        rv_list.setLayoutManager(llm);
+
+        rv_list.setAdapter(curriculumAdapter);
+
+        getData();
 
     }
 
     private void getData() {
     }
 
-    public void btnClick(View view) {
-        switch (view.getId()) {
-            case R.id.v1:
-                Intent intent1 = new Intent(this, CurriculumScheduleActivity.class);
-                intent1.putExtra("aa", 1);
-                startActivity(intent1);
-                break;
-            case R.id.v2:
-                Intent intent2 = new Intent(this, CurriculumScheduleActivity.class);
-                intent2.putExtra("aa", 2);
-                startActivity(intent2);
-                break;
-            case R.id.v3:
-                Intent intent3 = new Intent(this, CurriculumScheduleActivity.class);
-                intent3.putExtra("aa", 3);
-                startActivity(intent3);
-                break;
-        }
-    }
 }
